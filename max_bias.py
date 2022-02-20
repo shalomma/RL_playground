dda6f04ce4cca3abaca3e35b1fe315a6d21ace01
maayan.shalom <sha.maayan@gmail.com>
2022-02-21 01:08:02 +0200

    max_bias

---
+++ max_bias.py
@@ -0,0 +1,55 @@
+import numpy as np
+from gym import Env
+from gym.spaces import Discrete
+from q_learning import QLearning
+import matplotlib.pyplot as plt
+
+
+class MaxBiasEnv(Env):
+    def __init__(self):
+        self.state_space = Discrete(2)
+        self.action_space = Discrete(2)
+        self.state = 0
+
+    def step(self, action):
+        if self.state == 0 and action == 0:
+            reward = 0
+            done = True
+        elif self.state == 0 and action == 1:
+            self.state = 1
+            reward = 0
+            done = False
+        else:  # state == 1
+            reward = np.random.normal(loc=-0.1, scale=1.0)
+            done = True
+        return self.state, reward, done, {}
+
+    def reset(self):
+        self.state = 0
+
+    def render(self, mode="human"):
+        pass
+
+
+if __name__ == '__main__':
+    env = MaxBiasEnv()
+
+    n_epochs = 1000
+    action_epochs_history = []
+    for _ in range(n_epochs):
+        learner = QLearning(env, lr=0.1, gamma=1, eps=0.1)
+        n_episodes = 500
+        state_ = env.state
+        action_history = []
+        while n_episodes > 0:
+            state_, reward_, action_, done_ = learner.predict(state_)
+            if done_:
+                action_history.append(state_)
+                env.reset()
+                state_ = env.state
+                n_episodes -= 1
+        action_epochs_history.append(action_history)
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(np.array(action_epochs_history).mean(axis=0))
+    plt.show()
